0c311ea5fb982dd5066ee653cf240d6b872ef4da
yijun <yijun.jiang@lmitechnologies.com>
2022-01-13 14:48:36 -0500

    made changes for nordson app

--- efficientnet/gen_wts.py
+++ efficientnet/gen_wts.py
@@ -1,10 +1,19 @@
 import torch
 import struct
+import numpy as np
 from efficientnet_pytorch import EfficientNet
-model = EfficientNet.from_pretrained('efficientnet-b3')
 
+model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=5).to(torch.double)
+model.load_state_dict(torch.load('best.pt'))
 model.eval()
-f = open('efficientnet-b3.wts', 'w')
+
+dummy_input = np.ones((1,3,224,224))*0.1
+dummy_input = torch.from_numpy(dummy_input).to(torch.double)
+out = model(dummy_input)
+print(out)
+
+
+f = open('efficientnet-b0.wts', 'w')
 f.write('{}\n'.format(len(model.state_dict().keys())))
 for k, v in model.state_dict().items():
     vr = v.reshape(-1).cpu().numpy()
