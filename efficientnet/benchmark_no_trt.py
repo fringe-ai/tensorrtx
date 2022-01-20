c8021ee6e71a518e1ab1bb12cf8cb8fad3731c87
yijun <yijun.jiang@lmitechnologies.com>
2022-01-20 15:20:20 -0500

    modify benchmark

--- efficientnet/benchmark_no_trt.py
+++ efficientnet/benchmark_no_trt.py
@@ -5,6 +5,8 @@ import time
 from efficientnet_pytorch import EfficientNet
 
 NUM_IMAGES = 3
+DATA_TYPE = torch.half
+
 
 if torch.cuda.is_available():
     print('using GPU')
@@ -15,21 +17,21 @@ else:
 
 model = EfficientNet.from_pretrained('efficientnet-b0', num_classes=5).to(device)
 model.load_state_dict(torch.load('best.pt'))
-model = model.to(torch.half)
+model = model.to(DATA_TYPE)
 model.eval()
 
-dummy_input = np.ones((1,3,224,224))*0.1
-dummy_input = torch.from_numpy(dummy_input).to(torch.half).to(device)
 proc_times = []
-
 with torch.no_grad():
+    dummy_input = torch.zeros(1,3,224,224).to(DATA_TYPE).to(device)
     for i in range(20):
-        start_time = time.time()
+        t1 = time.time()
         for _ in range(NUM_IMAGES):
-                out = model(dummy_input)
-        proc_time = time.time()-start_time
-        print(f'{proc_time:.4f}')
-        proc_times.append(proc_time)
+            out = model(dummy_input)
+        diff = time.time()-t1
+        proc_times.append(diff*1000)
+        print(f'proc time: {diff:.4f}')
 proc_times = np.array(proc_times[1:])
 print(out)
-print(f'proc time: {proc_times.mean()*1000:.2f} ms')
+print(f'mean proc time: {proc_times.mean():.2f} ms')
+print(f'max proc time: {proc_times.max():.2f} ms')
+print(f'min proc time: {proc_times.min():.2f} ms')
